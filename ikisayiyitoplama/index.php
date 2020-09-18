c81d3d2f117e12823a6800b2cf056d2104fed7fb
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-09-18 10:26:51 +0300

    Create index.php

---
+++ ikisayiyitoplama/index.php
@@ -0,0 +1,24 @@
+<?php
+
+
+$sayi1 = 10;
+$sayi2 = 20;
+
+echo "Sayilarin Toplami : ",$sayi1+sayi2;
+
+?>
+
+
+// Veya 
+
+
+<?php
+
+$sayi1 = 10;
+$sayi2 = 20;
+
+$toplam = $sayi1+$sayi2;
+
+echo "Sayilarin Toplami : ",$toplam;
+
+?>
