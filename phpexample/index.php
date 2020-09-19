61a7cdc9f6aaac38ed66b0602cfc7892dcf3e288
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-09-19 03:19:27 -0700

    Create index.php

---
+++ phpexample/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Website 2 </title>
+</head>
+<body>
+
+    <script type="text/javascript" src="test.js"></script>
+    <link type="text/css" rel="stylesheet" href="test.css">
+    <input type="submit" onclick="flag();" value="İslemi Yap !!!">
+    <div id="İslemiYap"></div>
+</body>
+</html>
