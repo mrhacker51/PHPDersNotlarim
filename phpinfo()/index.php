c838541a388c5455d3e7caaad38fe2984f7d6a26
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-09-20 04:46:03 -0700

    Create index.php

---
+++ phpinfo()/index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP İnfo</title>
+</head>
+<body>
+    <?php
+
+    /* phpinfo() fonksiyonu  */
+    /* php nin o anki durumunu (Bilgilerini) ekrana yazar */ 
+    /* Degerleri : 
+    INFO_ALL     , Tüm Bilgileri Gösterir
+    INFO_LICENSE , Lisans Bilgilerini Sunar
+    INFO_CREDITS , Katkıda Bulunanların Bilgilerini Sunar
+    INFO_GENERAL , Genel Bilgiler
+    INFO_CONFIGURATION , Konfigürasyon Bilgileri
+    INFO_MODULES , Modül Bilgileri
+    INFO_ENVIRONMENT , Ortam Bilgileri
+    INFO_VARIABLES , $_SERVER SUPERGLOBALI Ön tanımlı Degisken
+    
+    */
+
+    # Örnek 1 : phpinfo();
+
+    # Örnek 2 : phpinfo(INFO_ALL);
+
+    # Örnek 3 : phpinfo(INFO_LICENSE);
+
+    # Örnek 4 : phpinfo(INFO_CREDITS);
+
+    # Örnek 5 : phpinfo(INFO_GENERAL);
+
+    # Örnek 6 : phpinfo(INFO_CONFIGURATION);
+
+    # Örnek 7 : phpinfo(INFO_MODULES);
+
+    # Örnek 8 : phpinfo(INFO_ENVIRONMENT);
+
+    # Örnek 9 : phpinfo(INFO_VARIABLES);
+
+
+    ?>
+</body>
+</html>
