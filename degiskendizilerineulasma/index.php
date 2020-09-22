9f2fa368eb92fc2a62674bb187c2217207960cf0
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-09-22 03:27:11 -0700

    Create index.php

---
+++ degiskendizilerineulasma/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $IsimSoyIsim = "TheBest Hacker";
+
+    if ($IsimSoyIsim[3] == "B" && ($IsimSoyIsim[5] == "s")){
+        echo "Hacker !!!";
+    }
+    else{
+        echo "Nope !";
+    }
+    ?>
+</body>
+</html>
