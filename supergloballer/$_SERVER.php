1568ea3fdf8fc514fc7be30f3b0828ab55cc0985
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-09-23 15:42:48 -0700

    Create $_SERVER.php

---
+++ supergloballer/$_SERVER.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>$_SERVER Süper Global</title>
+</head>
+         <body>
+            
+
+         <?php
+
+        echo $_SERVER["GATEWAY_INTERFACE"];
+
+        echo $_SERVER["SERVER_SOFTWARE"];
+        echo "</br>";
+        echo $_SERVER["PHP_SELF"];
+        echo "</br>";
+        echo $_SERVER["HTTP_USER_AGENT"];
+        echo "</br>";
+        echo $_SERVER["REMOTE_ADDR"];
+        echo "</br>";
+        echo $_SERVER["SERVER_ADDR"];
+        echo "</br>";
+        echo $_SERVER["REMOTE_PORT"];
+        echo "</br>";
+        echo $_SERVER["SERVER_PORT"];
+        echo "</br>";
+        echo $_SERVER["SERVER_PROTOCOL"];
+        echo "</br>";
+        echo $_SERVER["REQUEST_METHOD"];
+        echo "</br>";
+        echo $_SERVER["REQUEST_TIME"];
+        echo "</br>";
+        echo $_SERVER["QUERY_STRING"];
+        echo "</br>";
+        echo $_SERVER["DOCUMENT_ROOT"];
+        echo "</br>";
+        echo $_SERVER["SERVER_ADMIN"];
+        echo "</br>";
+        echo $_SERVER["SERVER_NAME"];
+
+
+        echo "</br>";
+        echo "</br>";
+        echo "</br>";
+        echo "</br>";
+        echo "</br>";
+
+
+
+
+        ?>  
+            
+
+
+        </body>
+</html>
