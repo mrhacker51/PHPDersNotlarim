99d669ebcfc4b64bb6d39679c9a10571e883aafb
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-09-28 16:14:47 -0700

    Create const.php

---
+++ sabitler/const.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sabitler Const</title>
+</head>
+<body>
+    
+    <?php
+
+    const ISIM = "Hacks";
+    const ISIM1 = "Stack";
+    const ISIM2 = "Over";
+    const ISIM3 = "Flow";
+
+
+
+    
+    /*
+    Sabitler oluşturulurken Büyük harflerle ile oluşturur genelde böyle kullanılırlar 
+
+    */
+
+    
+    echo ISIM;
+    echo ISIM1;
+    echo ISIM2;
+    echo ISIM3;
+    
+    
+    
+    
+    ?>
+</body>
+</html>
