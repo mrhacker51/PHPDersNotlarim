273369ba1536937f7884e3610cd3418d1e80ad78
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-09-28 16:04:42 -0700

    Create define.php

---
+++ sabitler/define.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sabitler Define</title>
+</head>
+<body>
+    <?php
+
+    define("name","Hacker");
+    define("surname","TheBest");
+    define("example1","php1");
+    define("example2","php2");
+    define("example3","php3");
+    define("number1",10);
+    define("number2",20);
+
+    echo name . "</br>";
+    echo surname. "</br>";
+    echo example1. "</br>";
+    echo example2. "</br>";
+    print(example3. "</br>");
+    print(number1. "</br>");
+    print(number2);
+
+    ?>
+</body>
+</html>
