4368883132b0b53e4f75c5964d6991bf492cf98a
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-06 15:53:49 -0700

    Create __LINE__.php

---
+++ sihirlisabitler/__LINE__.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>__LINE__</title>
+</head>
+<body>
+    
+
+<?php
+
+    $Line = __LINE__ . "</br>";
+    echo  "İlgili Kodun Satır Numarası : " . __LINE__ . "</br>";
+
+    echo  "İlgili Kodun Satır Numarası : " . $Line . "</br>";
+
+?>
+</body>
+</html>
