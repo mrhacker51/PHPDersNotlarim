ebde2c873e495fe873c1e9f7677b2dcf532cb89c
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-06 17:32:22 -0700

    Create __FILE__.php

---
+++ sihirlisabitler/__FILE__.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>__FILE__</title>
+</head>
+<body>
+    <?php
+    $File = __FILE__;
+
+    echo "Tam Dosya Yolunu Gösterir : " . $File . "</br>";
+
+    echo __FILE__;
+
+?>
+</body>
+</html>
