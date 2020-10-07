ba3212cde132f11d8b76becb38f7a23199ddc545
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-07 11:12:32 -0700

    Create __NAMESPACE__.php

---
+++ sihirlisabitler/__NAMESPACE__.php
@@ -0,0 +1,25 @@
+<?php
+    namespace Projeler;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>__NAMESPACE__</title>
+</head>
+<body>
+
+    <?php
+    /* __NAMESPACE__ = İsim uzayı Sınıflarla ve birçok aynı sınıf ismiyle calıstıgımız zaman bize oldukça kolaylık saglayan bir yapıya sahiptir .*/
+    /* Her zaman sayfanın en üstünde tanımlanır . */
+
+    echo __NAMESPACE__;
+
+    $Sonuc = __NAMESPACE__;
+    print($Sonuc);
+
+    ?>
+</body>
+</html>
