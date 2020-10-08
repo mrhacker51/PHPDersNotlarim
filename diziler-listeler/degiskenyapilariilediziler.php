cf8a407db3f744351b1ac1890b2b194c135615b9
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-08 13:44:07 -0700

    Create degiskenyapilariilediziler.php

---
+++ diziler-listeler/degiskenyapilariilediziler.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Degisken Yapılarıyla Dizi Tanımlama Advanced</title>
+</head>
+<body>
+    
+    <?php
+
+
+    /* Örnek */
+
+    $Response[] = "Pro";
+    $Response[] = "Pro2";
+    $Response[] = "Pro3";
+
+    echo "<pre>";
+    print_r($Response);
+    echo "</pre>";
+
+
+    $Response2[] = "Pro4";
+    $Response2[] = "Pro5";
+    $Response2[] = "Pro6";
+
+    echo "<pre>";
+    print_r($Response2);
+    echo "</pre>";
+
+    
+    ?>
+</body>
+</html>
