1fcba60390809c78614721e5981fa2681b4f0098
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-20 11:20:47 -0700

    Update switch,case,default.php

--- kosul_yapıları/switch,case,default.php
+++ kosul_yapıları/switch,case,default.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>switch,case,break,default</title>
+</head>
+<body>
+    <?php
 
+    $Saat = 10;
+
+
+    switch($Saat){
+    case (($Saat <= 10) and ($Saat >=20));
+        echo "Saat Suanda : {$Saat} ";
+    break;
+
+
+    case  (($Saat == 20) and ($Saat < 20));
+        echo "Saat Suanda : {$Saat}";
+    break;
+
+
+    case (($Saat == 10) and ($Saat < 20));
+        echo "Saat Suanda : {$Saat}";
+    break;
+
+    default:
+        echo "Saat Calismiyor ! ";
+    break;
+
+    }
+
+
+
+    ?>
+</body>
+</html>
