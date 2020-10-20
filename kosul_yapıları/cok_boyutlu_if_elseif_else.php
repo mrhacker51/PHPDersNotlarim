848b817944bd572b2532405cb4d953a1e3413cac
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-20 08:00:45 -0700

    Update cok_boyutlu_if_elseif_else.php

--- kosul_yapıları/cok_boyutlu_if_elseif_else.php
+++ kosul_yapıları/cok_boyutlu_if_elseif_else.php
@@ -1,1 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cok boyutlu if,elseif,else statement</title>
+</head>
+<body>
 
+    <?php
+
+    $Web = "www.google.com";
+    $Server = 100;
+    $Client = "appserver";
+
+
+    if (($Web != "www.hollandali.com") and ($Server != 200)){
+        if ($Client == "appserver"){
+            echo "Tebrikler ... ";
+        }
+
+        elseif (($Client != "appserver")){
+            echo "Not found ! ";
+        }
+
+        else{
+            echo "404 Error !!";
+        }
+    }
+
+    elseif (($Web == "www.nice.com") and ($Client == "Xammp")){
+        if (($Server == 200)){
+            echo "Oh nice ";
+        }
+        else{
+            echo "Server Error ! ";
+        }
+    }
+
+
+    else{
+        "Try Again .... [ Error ]";
+    }
+
+
+
+
+
+
+    ?>
+    
+</body>
+</html>
