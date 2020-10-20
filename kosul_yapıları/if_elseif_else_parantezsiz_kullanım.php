d939daf8327c67fee6b54647f80fae6026fa3de9
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-20 08:12:09 -0700

    Update if_elseif_else_parantezsiz_kullanım.php

--- kosul_yapıları/if_elseif_else_parantezsiz_kullanım.php
+++ kosul_yapıları/if_elseif_else_parantezsiz_kullanım.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>if,elseif,else süslü parantez olmadan kullanımı</title>
+</head>
+<body>
 
+    <?php
+        // : ve bitis icin ise endif; else icerde olucak sekilde 
+        if (5<10):
+            echo "Kosul Gecerli";
+        else:
+            echo "Kosul Gecersiz";
+        endif;
+
+    ?>
+</body>
+</html>
