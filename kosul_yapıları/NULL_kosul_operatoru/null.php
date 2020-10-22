3f1635c29aa4918d09b35b7ab628a31c597ee543
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-22 08:03:07 -0700

    Create null.php

---
+++ kosul_yapıları/NULL_kosul_operatoru/null.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NULL operatoru kullanımı ?? isaret</title>
+</head>
+<body>
+    <form action="sonuc.php" method="get">
+    
+    FormKullaniciAdi    : <input type="text" name="name"></br>
+    FormKullaniciSoyAdi : <input type="text" name="surname"></br>
+    <input type="submit" value="Gönder">    
+    
+    </form>
+
+    </br>
+
+    <form action="sonuc.php" method="post">
+    
+    FormKullaniciAdi    : <input type="text" name="name"></br>
+    FormKullaniciSoyAdi : <input type="text" name="surname"></br>
+    <input type="submit" value="Gönder">    
+    
+    </form>
+
+    </br>
+    
+    <form action="sonuc.php" method="post">
+    
+    FormKullaniciAdi    : <input type="text" name="name"></br>
+    FormKullaniciSoyAdi : <input type="text" name="surname"></br>
+    <input type="submit" value="Gönder">    
+    
+    </form>
+</body>
+</html>
