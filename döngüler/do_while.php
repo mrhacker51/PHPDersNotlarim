22660deadf6f1bd852dcae2607a52664f13ea09c
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-24 12:10:32 -0700

    Update do_while.php

--- döngüler/do_while.php
+++ döngüler/do_while.php
@@ -1,1 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>do while döngüsü</title>
+</head>
+<body>
 
+    <?php
+    // C programlama dilinde oldugu gibi aynı şekilde döngü 1 kere döner sonra kosula bakar ... 
+    $Deger = 1;
+
+    do{
+        echo "Deger : " . $Deger;
+        $Deger++;
+
+    }while($Deger <=10);
+    
+    
+    $Surname = "Temnikova";
+
+
+    do{
+        echo "You surname  : " . $Surname;
+        $Surname++;
+    }while($Surname === "Temnikova");
+    
+    
+    
+    ?>
+</body>
+</html>
