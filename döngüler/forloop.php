f9bd69e8e5db6dc1cc8030a8e114dbc590be48b6
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-24 14:19:25 -0700

    Update forloop.php

--- döngüler/forloop.php
+++ döngüler/forloop.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for döngüsü</title>
+</head>
+<body>
+    <?php
 
+    for ($Deger = 1; $Deger <= 100; $Deger++){
+        if (($Deger % 2 == 0)){
+            echo "Cift Sayilar : " . $Deger . "</br>";
+        }
+        else{
+            echo "Tek Sayilar : " . $Deger . "</br>";
+        }
+    }
+
+
+    for ($sayi = 10; $sayi >= 1; $sayi--){
+        echo "Degerler : {$sayi}" . "</br>";
+    }
+    ?>
+</body>
+</html>
