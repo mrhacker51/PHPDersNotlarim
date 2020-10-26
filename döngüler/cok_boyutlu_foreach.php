e35a4438bb829910e4b10af924e294afc2f5d8a8
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-10-26 21:28:17 +0300

    Update cok_boyutlu_foreach.php

--- döngüler/cok_boyutlu_foreach.php
+++ döngüler/cok_boyutlu_foreach.php
@@ -1,1 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>foreach döngüsü cok boyutlu kullanimi .... </title>
+</head>
+<body>
+    <?php
 
+    $My_list = array("PHP","Java","Javascript",array("Anna","Julia","Vesla",array("John","James","James")));
+
+
+    echo "<pre>";
+    print_r($My_list);
+    echo "</pre>";
+
+
+  // veya foreach($My_list[3] as $Icerik)
+  
+    foreach($My_list as $Icerik){
+    	if (is_array($Icerik)){
+    		foreach($Icerik as $Deger){
+    			echo $Deger;
+    			if (is_array($Deger)){
+    				foreach($Deger as $Degerler){
+    					echo $Degerler;
+    				}
+    			}
+
+    			else{
+    				echo $Deger;
+    			}
+    		}
+    	}
+
+    	else{
+    		echo $Icerik;
+    	}
+    }
+
+
+
+    ?>
+</body>
+</html>
